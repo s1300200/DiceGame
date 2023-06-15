0b3a326cd9a8dbbb837493f77c371db03a1c3599
kkoutasato <113033366+kkoutasato@users.noreply.github.com>
2023-06-15 15:25:02 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,7 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 public class DiceGame{
 
     public static void main(String[] args){
+        System.out.println("What is your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        System.out.println("Hello, " + name);
 
         System.out.println("Rolling dice...");
         Random rand1 = new Random();
